5496b1acdace192d4a1c417b72418428d74ac5a1
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-06-28 18:54:43 +0300

    commit tree a090b6bc66e0359ac7d540c9bf26fdc9010cf292

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+      return string.charAt(0).toUpperCase() + string.slice(1);
+ 
+  }
+  static sanitize(string){
+      return string.replace(/[^a-zA-Z0-9-_ ']/g, '');
+  }
+  static titleize(string){
+    // return string.replace(/\w\S*/g, (txt) => {
+    //   return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    //   });
+      const 
+  strs = new Set(['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']),
+  res = string
+    .split(" ")
+    .map((w) =>
+      strs.has(w.toLowerCase())
+        ? w.toLowerCase()
+        : w[0].toUpperCase() + w.slice(1).split().map((c) => c.toLowerCase())
+    )
+    .join(" ");
+    return res.charAt(0).toUpperCase() + res.slice(1);
+    }
 }
